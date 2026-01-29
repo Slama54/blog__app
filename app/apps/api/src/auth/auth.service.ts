7971d5f8fefe141f21d7c5e7d604a3d13646ea9b
Slama54 <wael.slama@email.com>
2026-01-29 15:15:05 +0100

    implement the validate local user and create the signin.input

--- app/apps/api/src/auth/auth.service.ts
+++ app/apps/api/src/auth/auth.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { SignInInput } from './dto/signin.input';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { verify } from 'argon2';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly prisma: PrismaService) {}
+  async validateLocalUSer({ email, password }: SignInInput) {
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+    });
+    if (!user || !user.password)
+      throw new UnauthorizedException('Invalid credentials');
+    const passwordMatch = await verify(user.password, password);
+    if (!passwordMatch) throw new UnauthorizedException('Invalid credentials');
+    return user;
+  }
+}
---
+++ app/apps/api/src/auth/dto/signin.input.ts
@@ -0,0 +1,10 @@
+import { InputType, Field } from '@nestjs/graphql';
+import { IsEmail } from 'class-validator';
+@InputType()
+export class SignInInput {
+  @Field()
+  @IsEmail()
+  email: string;
+  @Field()
+  password: string;
+}
