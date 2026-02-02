4b217f4869c12817d3b31d9bba4772dc1a01a6ff
Slama54 <wael.slama.issat@gmail.com>
2026-02-02 14:43:11 +0100

    grading the findAll users

--- app/apps/api/src/post/post.resolver.ts
+++ app/apps/api/src/post/post.resolver.ts
@@ -1,7 +1,9 @@
 /* eslint-disable prettier/prettier */
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Context } from '@nestjs/graphql';
 import { PostService } from './post.service';
 import { Post } from './entities/post.entity';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/gards/jwt-auth/jwt-auth.guard';
 
 
 @Resolver(() => Post)
@@ -9,9 +11,11 @@ export class PostResolver {
   constructor( private readonly postService: PostService) {}
 
 
-
+@UseGuards(JwtAuthGuard)
   @Query(() => [Post], { name: 'posts' })
-  async findAll() {
+  async findAll(@Context() context) {
+    const user = context.req.user;
+    console.log(user);
     return await this.postService.findAll();
   }
 
